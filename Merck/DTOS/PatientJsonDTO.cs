3066927a6b18d756d2fd383748cecef4a04007e3
MahdiHassan <Mahdi@microsysx.com>
2023-04-04 17:28:55 +0500

    Patient detail screen

---
+++ Merck/DTOS/PatientJsonDTO.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Merck.DTOS
+{
+    public class PatientJsonDTO
+    {
+        public int Id { get; set; }
+        public string DeviceId { get; set; }
+        public string Node { get; set; }
+        public DateTime LastTransmissionDate { get; set; }
+        public List<StepsDTO> Steps { get; set; }
+    }
+}
