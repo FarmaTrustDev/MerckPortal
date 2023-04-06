ffb09cf6fabf625f25fb9f11e0188e0b0f9fb0d5
MahdiHassan <Mahdi@microsysx.com>
2023-04-06 17:45:56 +0500

    dashboard json

--- Merck/Controllers/HomeController.cs
+++ Merck/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Merck.Models;
+﻿using Merck.DTOS;
+using Merck.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -18,8 +19,35 @@ namespace Merck.Controllers
             _logger = logger;
         }
 
+        public JsonResult DeviceJson()
+        {
+            List<DeviceDTO> devices = new List<DeviceDTO>()
+            {
+                new DeviceDTO
+                {
+                    Id= 1,
+                    Name= "Bot 1",
+                    Stats = new StatsDTO
+                    {
+                        NoOfTransmission = 12, DeviceTypes = 36, Distribution = 19, OverallAttacks = 9, TransmissionError = 3
+                    },
+                },
+                new DeviceDTO
+                {
+                    Id= 2,
+                    Name= "Bot 2",
+                    Stats = new StatsDTO
+                    {
+                        NoOfTransmission = 20, DeviceTypes = 26, Distribution = 30, OverallAttacks = 12, TransmissionError = 8
+                    },
+                }
+            };
+            return Json(devices);
+        }
         public IActionResult Index()
         {
+            var data = DeviceJson();
+            ViewBag.DeviceData = data;
             return View();
         }
 
---
+++ Merck/DTOS/StatsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Merck.DTOS
+{
+    public class StatsDTO
+    {
+        public int NoOfTransmission { get; set; }
+        public int DeviceTypes { get; set; }
+        public int Distribution { get; set; }
+        public int OverallAttacks { get; set; }
+        public int TransmissionError { get; set;}
+    }
+}
